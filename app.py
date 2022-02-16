181ffbff1c996d5773ba6cef55367a66461643b3
igi-chris <62021095+igi-chris@users.noreply.github.com>
2022-02-16 16:42:34 +0000

    first version

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from http.client import OK
 import os
 import joblib
 
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, KernelPCA, FastICA
 from flask import Flask, Response, make_response, request, send_file
 
 from pca.principle_component import build_interpretation_json, build_transformed_data_json
@@ -19,7 +19,8 @@ os.makedirs(models_dir, exist_ok=True)
 def connection_test() -> Response:
     return make_response("Request Successful", HTTPStatus.ACCEPTED)
 
-@app.route("/pca/train", methods=['POST'])
+
+@app.route("/decomposition/pca/train", methods=['POST'])
 def train_pca() -> Response:
     data = request.json
     model = PCA()
@@ -27,7 +28,23 @@ def train_pca() -> Response:
     return make_response(build_interpretation_json(model), HTTPStatus.ACCEPTED)
 
 
-@app.route("/pca/apply", methods=['POST'])
+@app.route("/decomposition/ica/train", methods=['POST'])
+def train_ica() -> Response:
+    data = request.json
+    model = FastICA()
+    model.fit(data)
+    return make_response(build_interpretation_json(model), HTTPStatus.ACCEPTED)
+
+
+@app.route("/decomposition/kernel-pca/train", methods=['POST'])
+def train_kernel_pca() -> Response:
+    data = request.json
+    model = KernelPCA()
+    model.fit(data)
+    return make_response(build_interpretation_json(model), HTTPStatus.ACCEPTED)
+
+
+@app.route("/decomposition/apply", methods=['POST'])
 def apply_pca() -> Response:
     try:
         ref = request.json["ref"]  # type: ignore
@@ -43,7 +60,7 @@ def apply_pca() -> Response:
     return make_response(resp_json, HTTPStatus.ACCEPTED)
 
 
-@app.route("/pca/get-model", methods=['GET'])
+@app.route("/decomposition/get-model", methods=['GET'])
 def get_serialised_model() -> Response:
     ref = request.args["ref"]   # TODO : handle err resp
     model = get_model(ref)    
@@ -53,7 +70,7 @@ def get_serialised_model() -> Response:
     return make_response(send_file(fpath, as_attachment=True), HTTPStatus.OK)
 
 
-@app.route("/pca/rebuild", methods=['Post'])
+@app.route("/decomposition/rebuild", methods=['Post'])
 def rebuild_model_from_file() -> Response:
     uploaded_file = request.files['file']
     if not uploaded_file.filename:
--- pca/principle_component.py
+++ pca/principle_component.py
@@ -1,11 +1,16 @@
 from dataclasses import dataclass
-from typing import Iterable, List, Dict, Optional
+from typing import Iterable, List, Dict, Optional, Union
 import json
 import uuid
+import math
+from numpy import isin
 
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, FastICA, KernelPCA
 
 from pca.model_register import register_model, has_model, get_model
+
+
+Decomp = Union[PCA, FastICA, KernelPCA]
 
 
 @dataclass()
@@ -18,7 +23,18 @@ class Component:
     eigenvector: List[float]
 
 
-def build_components(trained_model: PCA) -> Iterable[Component]:
+def build_components(trained_model: Decomp) -> Iterable[Component]:
+    if isinstance(trained_model, PCA):
+        return build_components(trained_model)
+    elif isinstance(trained_model, FastICA):
+        return build_ica_components(trained_model)
+    elif isinstance(trained_model, KernelPCA):
+        return build_kernel_pca_components(trained_model)
+    else:
+        raise NotImplementedError(f"Model type: {type(trained_model)} not supported.")
+
+
+def build_pca_components(trained_model: PCA) -> Iterable[Component]:
     """
     Translate PCA model results to collection of components
     ready for serialisation (for web response).
@@ -35,7 +51,30 @@ def build_components(trained_model: PCA) -> Iterable[Component]:
             eigenvector=trained_model.components_[i].tolist())
 
 
-def build_interpretation_json(model: PCA, ref: Optional[str] = None) -> str:
+def build_kernel_pca_components(trained_model: KernelPCA) -> Iterable[Component]:
+    cumulative_prop = 0
+    for i, ev in enumerate(trained_model.eigenvalues_):
+        yield Component(name=f"KPC{i+1}",
+            proportion=0,
+            cumulativeproportion=0,
+            singularvalue=math.sqrt(ev),
+            eigenvalue=ev,
+            eigenvector=trained_model.eigenvectors_[i].tolist())
+
+
+def build_ica_components(trained_model: FastICA) -> Iterable[Component]:
+    cumulative_prop = 0
+    for i, c in enumerate(trained_model.components_):
+        yield Component(name=f"IC{i+1}",
+        # zeros as these values are not given on an ICA model
+            proportion=0,
+            cumulativeproportion=0,
+            singularvalue=0,
+            eigenvalue=0,
+            eigenvector=c.tolist())
+
+
+def build_interpretation_json(model: Decomp, ref: Optional[str] = None) -> str:
     if ref is None or not has_model(ref):
         ref = register_model(model)
         
