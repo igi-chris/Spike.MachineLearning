e77bfb413fbbd24e0b459dcc132f860217a8a32a
igi-chris <62021095+igi-chris@users.noreply.github.com>
2022-05-23 17:19:57 +0100

    lookup filepath from ref

--- common/utils.py
+++ common/utils.py
@@ -1,14 +1,25 @@
-import csv
 import os
 import re
-from typing import Sequence
 import unicodedata
 
+from literals import base_dir, tmp_files_dir_name, training_data_fname
 
-def get_csv_headers(file_path: str) -> Sequence[str]:
-    with open(file_path, 'r') as f:
-        d_reader = csv.DictReader(f)
-        return d_reader.fieldnames if d_reader.fieldnames is not None else []
+
+def ref_from_path(path: str) -> str:
+    pattern = "[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}"
+    regex = re.compile(pattern, re.I)
+    res = regex.match(path)
+    if res is None:
+        raise ValueError(f"Unable to find GUID in path: {path}")
+    return res.group(1)
+
+
+def csv_path_from_ref(ref: str) -> str:
+    tmp_files_dir = os.path.join(base_dir, tmp_files_dir_name)
+    os.makedirs(tmp_files_dir, exist_ok=True)
+    target_dir = os.path.join(tmp_files_dir, ref)
+    os.makedirs(target_dir, exist_ok=True)
+    return os.path.join(target_dir, training_data_fname)
 
 
 def secure_filename(filename: str) -> str:
