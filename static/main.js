74db50d22ccdcb270e88bdab560e3aa668967cbb
igi-chris <62021095+igi-chris@users.noreply.github.com>
2022-05-26 10:30:22 +0100

    fixed deselect

--- static/main.js
+++ static/main.js
@@ -135,16 +135,21 @@ function showOrHideConstValueField() {
 function selectExperiment(id) {
     el = document.getElementById("selected-experiment-id")
     if (el.value == id) {
-        // deselect
-        el.value = null;
-        expEl = document.getElementById(`exp-${id}`);
-        expEl.classList.remove('selected-experiment')
-        console.log(`selected-experiment-id reset to: ${document.getElementById("selected-experiment-id").value}`);
+        deselectExperiment()
     } else {
         el.value = id;
         document.args_form.submit();
         console.log(`selected-experiment-id set to: ${document.getElementById("selected-experiment-id").value}`);
     }
+}
+
+function deselectExperiment() {
+    el = document.getElementById("selected-experiment-id")
+    id = el.value;
+    el.value = null;
+    expEl = document.getElementById(`exp-${id}`);
+    expEl.classList.remove('selected-experiment');
+    expEl.blur();  // remove focus from element
 }
 
 function saveModel(model_type) {
