396e9fee46b6ed2eefe0d23bca4131eec8b794f1
igi-chris <62021095+igi-chris@users.noreply.github.com>
2022-06-21 20:03:10 +0100

    Update experiment.js

--- static/modules/experiment.js
+++ static/modules/experiment.js
@@ -1,21 +1,29 @@
 export function selectExperiment(id) {
-    el = document.getElementById("selected-experiment-id")
+    var el = document.getElementById("selected-experiment-id")
     if (el.value == id) {
         deselectExperiment()
     } else {
         el.value = id;
-        document.args_form.submit();
+        document.args_form.submit();  // reload with selected experiment
         console.log(`selected-experiment-id set to: ${document.getElementById("selected-experiment-id").value}`);
     }
 }
 
-function deselectExperiment() {
-    el = document.getElementById("selected-experiment-id")
-    id = el.value;
+export function deselectExperiment() {
+    var el = document.getElementById("selected-experiment-id")
+    var id = el.value;
     el.value = null;
-    expEl = document.getElementById(`exp-${id}`);
+
+    var expEl = document.getElementById(`exp-${id}`);
     if (expEl) {
         expEl.classList.remove('selected-experiment');
         expEl.blur();  // remove focus from element        
     }
+}
+
+export function highlightSelectedExperiment() {
+    var selectedId = document.getElementById("selected-experiment-id");
+    if (!selectedId || !selectedId.value) { return; }
+    var selExpBtn = document.getElementById(`exp-${selectedId.value}`);
+    selExpBtn.classList.add('selected-experiment')
 }
