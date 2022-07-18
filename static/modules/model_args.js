da7eda6e38d16488e95af1ddb24021d3c979dab7
igi-chris <62021095+igi-chris@users.noreply.github.com>
2022-07-18 11:03:45 +0100

    Create model_args.js

---
+++ static/modules/model_args.js
@@ -0,0 +1,8 @@
+//export function get_model_args()
+
+// problem: we have python art types
+//          need to req from js
+//          and either show/hide chg from js 
+
+// or
+// re-render & get from python / use jinja???  - this for now
