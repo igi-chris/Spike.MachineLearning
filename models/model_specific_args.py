1e2a7d2dc6b8283875956c181efe214f93854bb1
igi-chris <chris.prosser@igiltd.com>
2022-07-18 07:42:02 +0100

    building arg types

--- models/model_specific_args.py
+++ models/model_specific_args.py
@@ -1,13 +1,39 @@
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple
 from enum import Enum
+from abc import ABC
 
 from dataclasses import dataclass, field
-from urllib.robotparser import RobotFileParser
 
 
 class Kernel(Enum):
     RBF = 0
     Matern = 1
+
+
+@dataclass
+class ModelArgument(ABC):
+    arg_name: str
+    parent_name: Optional[str] = field(default=None)
+
+
+class SelectionArg(ModelArgument):
+    arg_name: str
+    options: List[str] = field(default_factory=list)
+    default: Optional[str] = field(default=None)
+    parent_name: Optional[str] = field(default=None)
+
+
+class NumericalArg(ModelArgument):
+    arg_name: str
+    default: Optional[float] = field(default=None)
+    parent_name: Optional[str] = field(default=None)
+
+
+class RangeArg(ModelArgument):
+    arg_name: str
+    default: Optional[Tuple[float, float]] = field(default=None)
+    parent_name: Optional[str] = field(default=None)
+
     
 
 
@@ -15,6 +41,8 @@ class Kernel(Enum):
 class RBFKernel:
     length_scale: float = field(default=1.0)
     length_scale_bounds: Tuple[float, float] = field(default=(1e-05, 100000.0))
+
+
 
 
 
