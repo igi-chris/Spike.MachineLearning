1a551f67f8657ce03045d149cc0d8cdf98f434e2
igi-chris <62021095+igi-chris@users.noreply.github.com>
2022-07-21 09:45:49 +0100

    added default kernel type & ids

--- models/model_specific_args/arg_register.py
+++ models/model_specific_args/arg_register.py
@@ -3,12 +3,12 @@ Defines which args go with which models
 """
 from typing import Dict, List
 
-from models.model_specific_args.arg_types import ModelArgument, gaussian_process_kernel
+from models.model_specific_args.arg_types import BaseModelArgument, gaussian_process_kernel
 
 
-_model_to_args_map: Dict[str, List[ModelArgument]] = {
+_model_to_args_map: Dict[str, List[BaseModelArgument]] = {
     "GaussianProcessRegressor": [gaussian_process_kernel]
 }
 
-def get_model_args(model_name: str) -> List[ModelArgument]:
+def get_model_args(model_name: str) -> List[BaseModelArgument]:
     return _model_to_args_map.get(model_name, [])
--- models/model_specific_args/arg_types.py
+++ models/model_specific_args/arg_types.py
@@ -5,37 +5,19 @@ from dataclasses import dataclass, field
 
 
 @dataclass
-class ModelArgument(ABC):
+class BaseModelArgument(ABC):
     field_type: ClassVar[str]
-
-
-@dataclass
-class StringSelectionArg(ModelArgument):
-    field_type: ClassVar[str] = "selection"
     display_name: str
-    default_value: Optional[str] = field(default=None)
-    options: List[str] = field(default_factory=list)
 
+    @property
+    def id(self) -> str:
+        """Get the id that can be used by default in the html"""
+        return self.display_name.replace(" ", "-")
 
-@dataclass
-class NumericalArg(ModelArgument):
-    field_type: ClassVar[str] = "number"
-    display_name: str
-    default_value: Optional[float] = field(default=None)
-
-
-@dataclass
-class RangeArg(ModelArgument):
-    field_type: ClassVar[str] = "range"
-    display_name: str
-    default_value: Optional[Tuple[float, float]] = field(default=None)
-
-
-@dataclass
-class NestedArgs(ModelArgument):
-    field_type: ClassVar[str] = "group"
-    display_name: str
-    children: List[ModelArgument]
+    @property
+    def dom_name(self) -> str:
+        """The html element name is used for the arg name when submitting the form"""
+        return self.display_name.replace(" ", "_")
 
     def __str__(self):
         """Needed for dropdowns where selections are complex, can just call str() on any selection now"""
@@ -43,11 +25,57 @@ class NestedArgs(ModelArgument):
 
 
 @dataclass
-class ComplexSelectionArg(ModelArgument):
+class StringSelectionArg(BaseModelArgument):
+    field_type: ClassVar[str] = "selection"
+    display_name: str
+    default_value: Optional[str] = field(default=None)
+    options: List[str] = field(default_factory=list)
+
+    @property
+    def id(self) -> str:
+        """Get the id that can be used by default in the html"""
+        return super().id + "-options"
+
+
+@dataclass
+class NumericalArg(BaseModelArgument):
+    field_type: ClassVar[str] = "number"
+    display_name: str
+    default_value: Optional[float] = field(default=None)
+
+
+@dataclass
+class RangeArg(BaseModelArgument):
+    field_type: ClassVar[str] = "range"
+    display_name: str
+    default_value: Optional[Tuple[float, float]] = field(default=None)
+
+
+@dataclass
+class NestedArgs(BaseModelArgument):
+    field_type: ClassVar[str] = "group"
+    display_name: str
+    children: List[BaseModelArgument]
+
+
+@dataclass
+class TextInPlaceOfArgs(BaseModelArgument):
+    field_type: ClassVar[str] = "display"
+    display_name: str  # e.g. for dropdown
+    text: str
+
+
+@dataclass
+class ComplexSelectionArg(BaseModelArgument):
     field_type: ClassVar[str] = "selection-nested"
     display_name: str
-    default_value: Optional[ModelArgument] = field(default=None)
-    options: List[ModelArgument] = field(default_factory=list)
+    default_value: Optional[BaseModelArgument] = field(default=None)
+    options: List[BaseModelArgument] = field(default_factory=list)
+
+    @property
+    def id(self) -> str:
+        """Get the id that can be used by default in the html"""
+        return super().id + "-options"
 
 
 length_scale = NumericalArg(display_name="Length Scale", default_value=1.0)
@@ -55,10 +83,14 @@ length_scale_bounds = RangeArg(display_name="Length Scale Bounds",
                                default_value=(1e-05, 100000.0))
 nu = NumericalArg(display_name="nu (smoothness)")
 
-kernel_rbf = NestedArgs(display_name="RBF", children=[length_scale, length_scale_bounds])
-kernel_matern = NestedArgs(display_name="Matern", children=[length_scale, length_scale_bounds, nu])
+kernel_default = TextInPlaceOfArgs(display_name="Default",
+    text="ConstantKernel(1.0, constant_value_bounds='fixed') * RBF(1.0, length_scale_bounds='fixed')")
+kernel_rbf = NestedArgs(display_name="RBF", 
+    children=[length_scale, length_scale_bounds])
+kernel_matern = NestedArgs(display_name="Matern", 
+    children=[length_scale, length_scale_bounds, nu])
 
 gaussian_process_kernel = ComplexSelectionArg("Kernel",
-                                              default_value=None,
-                                              options=[kernel_rbf, kernel_matern])
+    default_value=None,
+    options=[kernel_default, kernel_rbf, kernel_matern])
 
